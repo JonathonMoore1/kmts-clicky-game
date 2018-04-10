af1869fa914467a9030ce3907d828bfd232aabd7
Jonathon Moore <jonmoore510@gmail.com>
2018-04-09 21:11:26 -0500

    Added images.json object, images for click items, converted App function to class

--- kmts-clicky-game/src/App.js
+++ kmts-clicky-game/src/App.js
@@ -1,40 +1,32 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// s
 
-const App = () => {
-  return (
-    <div>
-      <nav>
-        <div className="navbar">
-          <ul>
-            <li className="brand">
-              <a href="/">Trailer Park Boys</a>
-            </li>
-            <li>
-              Click an image to begin!
-            </li>
-            <li>
-              Score: 0 | Top Score: 0
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <header className="header">
-        <h1>Clicky Game!</h1>
-        <h2>Click on an image to win points, but don't click on the same image more than once.</h2>
-      </header>
-      {/* <div className="container">
-        <main>
-          <div className="card" style="width: 18rem;">
-            <div className="card-body">
-              <img className="card-img" src="./img/ricky.jpg" alt="Card image cap"> />
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="/">
+          <img src={require("./img/sickle-and-hammer.png")} className="d-inline-block align-top mr-2" alt="" />
+            Karl Marx and Tiger Sharks!
+          <img src={require("./img/shark.png")} className="d-inline-block align-top ml-2" alt="" />
+          </a>
+        </nav>
+        <header className="header">
+          <h1>Clicky Game!</h1>
+          <h2>Click on an image to win points, but don't click on the same image more than once.</h2>
+        </header>
+        <div className="container">
+          <main>
+            <div className="card click-image">
+              <div className="card-body">
+                <img className="card-img" src="./img/ricky.jpg" alt="Card image cap" />
+              </div>
             </div>
-          </div>
-        </main>
-      </div> */}
-    </div>
-  )
+          </main>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
